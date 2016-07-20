278728bd866c632bcaf2985ddc3c1f1f037d6b08
nasa <nasa.freaks@gmail.com>
2016-07-20 12:24:00 +0700

    add static, templates, and views

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Home Page')
+
+
+
+@app.route('/couriers')
+def courires():
+    return "Couriers Page"
