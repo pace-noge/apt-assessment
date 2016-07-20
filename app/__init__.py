02c1fce9d0a867da677431df20e7d2cf449e61dc
nasa <nasa.freaks@gmail.com>
2016-07-20 17:06:03 +0700

    add flask login to requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
+import os
+from config import BASE_DIR
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init._app(app)
 
 
 from app import views, models
