6e6ff84be84a79eaed4ff4699482e7fbdadc03c3
nasa <nasa.freaks@gmail.com>
2016-07-20 23:21:27 +0700

    add courier detail page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app, db, lm
 from .forms import LoginForm, CourierForm
 from .models import User, Courier, Item, DeliveryJob
+from datetime import time
 from flask import render_template, flash, redirect
 from flask_login import login_user, logout_user, current_user, login_required
 
@@ -35,15 +36,58 @@ def login():
 
 @app.route('/couriers', methods=['GET', 'POST'])
 @app.route('/couriers/', methods=['GET', 'POST'])
+@login_required
 def courier_list():
     form = CourierForm()
     if form.validate_on_submit():
-        pass
+        name = form.name.data
+        address = form.address.data
+        available_time_start = form.available_time_start.data
+        available_time_stop = form.available_time_stop.data
+        courier = Courier(
+            name=name,
+            address=address,
+            available_time_start=available_time_start,
+            available_time_stop=available_time_stop,
+            creator=current_user,
+            last_modified_by=current_user
+        )
+        db.session.add(courier)
+        db.session.commit()
+        return redirect('/couriers/')
+    else:
+        flash_error(form)
+
     couriers = Courier.query.all()
     return render_template('courier.html', page_title="Couriers", couriers=couriers, form=form)
+
+
+@app.route('/couriers/<id>/', methods=['GET', 'POST'])
+@login_required
+def courier_detail(id):
+    courier = Courier.query.get(id)
+    if courier is not None:
+        return render_template('courier_detail.html', courier=courier, page_title=courier.name)
+    else:
+        return page_not_found(courier)
+
+
 
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect('/login')
+
+
+def flash_error(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                  getattr(form, field).label.text,
+                  error
+            ))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
